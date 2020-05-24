5d4c1de6ab9ec978eb940a493c46a73577ec459f
james <james@jibaoviewer.com>
2020-05-24 02:17:01 -0700

    [update] apply aug to crop video

--- utils/video_process.py
+++ utils/video_process.py
@@ -4,21 +4,31 @@
 import cv2
 import numpy as np
 import sys
-
+from augmentation import denoise,flip,blurring,increase_brightness
+import shutil
+import os
 ROOT =  "/Users/james/Pictures/opencv_test/record2/"
-OUTPUT_DIRECTORY = "d2/edited/"
+OUTPUT_ROOT =  "/Users/james/Pictures/opencv_test/input3/"
+
+
 INPUT_DIRECTORY = "d2/"
 INPUT_FILE = "d_2_t.mp4"
 SIZE_H = 280 
 SIZE_W = 280 
 
+OFFSET = 50
 DIS_TO_CENTER = (200,-240) # for top
+P_COPY_NUM = 5 # 0~4
+Filter_COPY_NUM = 4 #~0~4
 
 OUTPUT_FILE = "_r"
+
+# DIS_TO_CENTERS = [(200,-240)]
+
+
 STATUS = 1 # 1 output, 0 check
 
 fgbg = cv2.createBackgroundSubtractorMOG2()
-cap = cv2.VideoCapture(ROOT+INPUT_DIRECTORY+INPUT_FILE)
 
 # (x, y, w, h) = cv2.boundingRect(c)
 # cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 20)
@@ -51,21 +61,38 @@ def parseArgs():
                 SIZE_W = sys.argv[i]
             i+= 1
     # print(INPUT_FILE,OUTPUT_FILE,SIZE)
-def cropVideo():
+def cropVideo(type = 0,filter = 0):
     
+    input_path = ROOT+INPUT_DIRECTORY+INPUT_FILE
+    cap = cv2.VideoCapture(input_path)
 
     width = int(cap.get(3))
     height = int(cap.get(4))
     size_h = SIZE_H
     size_w = SIZE_W
-    dis_to_center = DIS_TO_CENTER
+    dis_to_center = getDis(DIS_TO_CENTER,OFFSET,type)
     dot_position = INPUT_FILE.rfind(".")
-    
+    print(dis_to_center)
     fourcc = cv2.VideoWriter_fourcc(*'MPEG')
     
     fps = cap.get(cv2.CAP_PROP_FPS)
     print("fps:",fps)
-    out = cv2.VideoWriter(ROOT+OUTPUT_DIRECTORY+INPUT_FILE[:dot_position]+OUTPUT_FILE+'.mp4',fourcc, fps, (SIZE_W, SIZE_H))
+
+    filename = "{}{}_p{}_f{}.mp4".format(INPUT_FILE[:dot_position],OUTPUT_FILE,str(type),str(filter))
+    output_path = "{}{}{}".format(OUTPUT_ROOT,INPUT_DIRECTORY,filename)
+
+    dot_position = filename.rfind(".")
+    output_labelname = "{}.csv".format(filename[:dot_position])
+    dot_position = input_path.rfind(".")
+    input_label_path = "{}.csv".format(input_path[:dot_position])
+    output_label_path = "{}{}labels/{}".format(OUTPUT_ROOT,INPUT_DIRECTORY,output_labelname)
+    
+    # print(input_path)
+    # print(output_path)
+    # print(input_label_path)
+    # print(output_label_path)
+
+    out = cv2.VideoWriter(output_path,fourcc, fps, (SIZE_W, SIZE_H))
     while True:
         ret, frame = cap.read()
         if not ret:
@@ -87,11 +114,18 @@ def cropVideo():
             print(len(image),len(image[0]),len(image[0][0]), (SIZE_W, SIZE_H))
 
             break
+        image = applyFilter(filter, image)
         out.write(image)
         if cv2.waitKey(1) == 27:
             break
     out.release()
     cv2.destroyAllWindows()
+
+    # copy label
+    copyLabel(input_label_path,output_label_path)
+
+
+
 
 def showRec():
 
@@ -169,11 +203,48 @@ def drawAllRec():
             break
     cv2.destroyAllWindows()
 
+def copyLabel(input_path,output_root):
+    print("[generateDataWithAug]:",input_path,output_root)
+    if not os.path.exists(input_path):
+        return
+    shutil.copy2(input_path,output_root)
+    print("Completed copying label: ",input_path)
+
+def getDis(position,offset,type):
+    if type == 4:
+        return (position[0]+offset,position[1]+offset) # for 4
+    elif type == 3:
+        return (position[0]-offset,position[1]+offset) # for 3
+    elif type == 2:
+        return (position[0]+offset,position[1]-offset) # for 2
+    elif type == 1:
+        return (position[0]-offset,position[1]-offset) # for 1
+    else:
+        return position
+
+def applyFilter(type,frame):
+    if type == 1:
+        return flip(frame)
+    elif type == 2:
+        return blurring(frame)
+    elif type == 3:
+        return increase_brightness(frame)
+    else:
+        return frame
+def generateDataWithAug():
+    print("generateDataWithAug")
+    for i in range(P_COPY_NUM):
+        for j in range(Filter_COPY_NUM):
+            cropVideo(i,j)
+
+def runningScript():
+    print("runningScript")
 if __name__ == "__main__":
 
     # parseArgs()
     # drawRec()
     if STATUS:
-        cropVideo()
+        # cropVideo()
+        generateDataWithAug()
     else:
         showRec()
