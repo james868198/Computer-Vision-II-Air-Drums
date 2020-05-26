b54e0c6fce47c3c0fe100e9adebb1a3cf9ca8893
james <james@jibaoviewer.com>
2020-05-25 19:10:50 -0700

    [add] add optical flow to batch_generate

--- utils/batch_generate.py
+++ utils/batch_generate.py
@@ -3,7 +3,7 @@ import cv2
 import time
 import os
 
-DATA_ROOT = "../../Data/input/"
+DATA_ROOT = "../../Data/test_input/"
 LABEL_ROOT = "labels/"
 SHAPE = (224, 224)
 INPUT_FILE_TYPE = {
@@ -14,19 +14,20 @@ INPUT_FRAME_NUMBER = 5
 
 TEST_ROOT = "../../Data/test/"
 
-def generateBatches(directory,shape = SHAPE):
+def generateBatches(directory,shape = SHAPE, of = False):
     print("[generateBatchs]")
     entries = os.listdir(directory)
     print(entries)
     batchs = []
     for file in entries:
-        data = generateBatch(directory,file,shape)
+        data = generateBatch(directory,file,shape,of)
         if data == None:
             continue
         batchs.extend(data)
+    print("[generateBatchs] end, total:",len(batchs))
     return batchs
 
-def generateBatch(directory,file, shape):
+def generateBatch(directory,file, shape, of = False):
 
     batches = []
     input_path = directory+file
@@ -58,17 +59,18 @@ def generateBatch(directory,file, shape):
         return 
     
     # start parsing
-    print("[generateBatchs] start parsing")
+    # print("[generateBatchs] start parsing")
     cap = cv2.VideoCapture(input_path)
 
     queue = []
     count = 0
     hit_count = 0 
     non_hit_count = 0
+    prev_frame = []
     while(cap.isOpened()):
         # Capture frame-by-frame
         ret, frame = cap.read()
-    
+       
         if ret:
             # print(str(count))
             if len(queue) == INPUT_FRAME_NUMBER:
@@ -76,6 +78,16 @@ def generateBatch(directory,file, shape):
             # check = isDrum(frame)
             if shape is not None: 
                 frame = cv2.resize(frame, shape)
+            
+            if of:
+                if type(prev_frame) == 'NoneType':
+                    count += 1
+                    prev_frame = frame
+                    continue
+                of_frame = opticalFlow(prev_frame, frame)
+                prev_frame = frame
+                frame = of_frame
+
             queue.append(frame)
             batch = queue.copy()
             if len(batch) == INPUT_FRAME_NUMBER:
@@ -93,7 +105,7 @@ def generateBatch(directory,file, shape):
             count += 1
         else:
             break
-    print("Sampled batches size:", len(batches))
+    # print("Sampled batches size:", len(batches))
     # When everything done, release the capture
     cap.release()
     # cv2.destroyAllWindows()
@@ -159,14 +171,30 @@ def saveBatch(type,seq):
             filename = dir_path + "/d{}_{}_{}.jpg".format(type,seq,i)
             cv2.imwrite(filename, batch[i])
 
+def opticalFlow(frame1,frame2):
+    try:
+        hsv = np.zeros_like(frame1)
+        hsv[...,1] = 255
+        prvs = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
+        next = cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
+        flow = cv2.calcOpticalFlowFarneback(prvs,next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+        mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
+        hsv[...,0] = ang*180/np.pi/2
+        hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
+        bgr = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
+        return bgr
+    except:
+        print("opticalFlow exception occurred")
+        return 
+
 if __name__ == "__main__":
     
     
     # filename = "labeledvideo5.mp4"
 
     # ---- generate batchs ----
-    batches = generateBatches(DATA_ROOT)
-    print("batches:",len(batches))
+    batches = generateBatches(DATA_ROOT, of = True)
+    # print("batches:",len(batches))
    
     # ---- plot batches ----
     # i = 1
